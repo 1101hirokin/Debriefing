61450c19791cf19be43d612e00fccae8af69b297
中谷仁貴 <nakatani-hiroki@Nakatani-Hiroki-MacbookPro.local>
2019-12-19 01:22:08 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+html {
+    font-size: 16px;
+}
+html, body {
+    padding: 0;
+    margin: 0;
+}
+.centering {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+
+}
+.vertical {
+    display: flex;
+    align-items: center;
+    align-content: center;
+}
+</style>
