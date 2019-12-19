76bbe92a6a82e2ad96617ba5501ed3d0bd97b464
中谷仁貴 <nakatani-hiroki@Nakatani-Hiroki-MacbookPro.local>
2019-12-19 22:45:11 +0900

    12/19 5th

--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/img/favicons/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/Debriefing/image/favicons/fav.ico' }
     ],
   },
   data() {
