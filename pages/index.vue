ab7d2ca5c769c45a8e13e4d0fe02a53b1472c141
中谷仁貴 <nakatani-hiroki@Nakatani-Hiroki-MacbookPro.local>
2019-12-19 20:58:33 +0900

    12/19 1st

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div id="websiteContainer">
     <div id="websiteContainer_inner">
         <div id="hirokiNakataniDeliverable">
+            <!--
             <div id="deliverableIconWrapper" class="">
                 <svg id="deliverableIcon" data-name="layor_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 704 704">
                     <path d="M159.47,242.67a160.9,160.9,0,0,1,23.75-18.14,173.29,173.29,0,0,1,21.61-11.65h0c29.7-14.87,41.29-12.4,50.82-22.77.5-.54.92-1,.92-1,1.8-2.36,3.09-6.2.87-17.8-.61-3.14-1.26-5.63-2.54-10.49-1.37-5.18-2.56-9.29-3.17-11.35-.17-.57-.29-1-.32-1.07-3-10.13-9.18-30.85-5.27-33.53,7.82-5.35,65.53,55,72.61,51.58a2.53,2.53,0,0,0,.26-.14c.85-.62,1.4-3.2.52-18.24-2-33.55-5.76-37.77-5.07-54.84.14-3.51-5.45-82.13,7.1-90.9,19.68-13.74,79.79,37.87,96.32,54.17,6.47,6.39,67.47,64,77.69,75.12,0,0,31.36,32,38.14,38.65,15.2,15,53.71,70.33,59.19,80.45a462.6,462.6,0,0,1,31,70.65c1.61,4.73,10.18,37.86,11.41,52.3,1.42,16.57,3.12,36.56-.25,62.65a251.23,251.23,0,0,1-19.28,69c-1.68,3.91-22,43-24.84,47.57-13.65,21.76-31.07,39-41,48.69a368.5,368.5,0,0,1-66.32,51.6s-29.26,17.65-62.2,29.8c-1.7.63-2.95,1.07-3.92,1.42-21.22,7.59-43.88,15.68-70.57,18.39-16.66,1.68-29.41,2.47-41.46-2.7-10.26-4.4-17.29-11.09-22.4-15.94-7-6.62-5.89-7.38-22.28-29.56-9.33-12.61-20.92-27.61-27.79-41.15-14.66-28.85-16.68-26-18-27.48-8.14-9.19-13.48-11.4-41.26-27.61-38-22.13-48.65-30-59.08-39.95-.21-.19-8.8-8.6-10.57-10.6-6.14-6.94-24.7-27.74-27.8-33.14a66.53,66.53,0,0,1-8.77-25.69,59.92,59.92,0,0,1,1.23-21.6c2.65-10.22,18.12-31,36.74-43.42.3-.2,1-.64,1.87-1.31A45.45,45.45,0,0,0,118,371.29a30,30,0,0,0,4-13.08c3.88-36.09,1.36-45,8.41-69.18C137.81,263.72,154.66,247.26,159.47,242.67Z"/>
@@ -9,6 +10,11 @@
             </div>
             <div id="deliverableStrWrapper">
                 <p id="deliverableStr">A HIROKI NAKATANI<br>DELIVERABLE</p>
+            </div>
+            -->
+
+            <div id="deliverableLogoWrapper">
+              <img src="@/assets/img/topLogo.svg" id="deliverableLogo" />
             </div>
         </div>
         <div id="topLogoContainer">
@@ -89,12 +95,15 @@ export default {
     }
 }
 #hirokiNakataniDeliverable {
+    /*
     $height: 60px;
-
     display: grid;
     grid-template-rows: minmax($height, auto);
     grid-template-columns: auto 1fr;
     column-gap: 8px;
+    */
+    display: block;
+    width: 100%;
 
     overflow: hidden;
 
@@ -102,6 +111,14 @@ export default {
     margin-bottom: 54px;
 
     $color: #2b2b2b;
+    #deliverableLogoWrapper {
+        height: 30px;
+        #deliverableLogo {
+          display: block;
+          height: 100%;
+        }
+    }
+    /*
     #deliverableIconWrapper {
         grid-row: 1/2;
         grid-column: 1/2;
@@ -126,6 +143,7 @@ export default {
             color: $color;
         }
     }
+    */
 }
 #topLogoContainer {
   margin-bottom: 54px;
