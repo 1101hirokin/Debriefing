c2be141e4bd93236243ba3a927130d9e6a2ce60c
中谷仁貴 <nakatani-hiroki@Nakatani-Hiroki-MacbookPro.local>
2019-12-21 00:14:58 +0900

    12/20 7th

--- docs/top/script/service-worker.js
+++ docs/top/script/service-worker.js
@@ -10,15 +10,11 @@ self.addEventListener('fetch', function (e) {
   
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { 
-        
-        return request
-      } else {       
-        
-        return fetch(e.request)
-      }
-      
-      
+		if (request) { 
+			return request
+		} else {
+			return fetch(e.request)
+		}
     })
   )
 })
@@ -27,7 +23,7 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       
-      return cache.addAll(URLS)
+      	return cache.addAll(URLS)
     })
   )
 })
@@ -35,19 +31,18 @@ self.addEventListener('install', function (e) {
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      
-      
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      
-      cacheWhitelist.push(CACHE_NAME)
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
+
+		var cacheWhitelist = keyList.filter(function (key) {
+			return key.indexOf(APP_PREFIX)
+		})
+		
+		cacheWhitelist.push(CACHE_NAME)
+		
+		return Promise.all(keyList.map(function (key, i) {
+			if (cacheWhitelist.indexOf(key) === -1) {
+				return caches.delete(keyList[i])
+			}
+		}))
+	})
   )
 })
